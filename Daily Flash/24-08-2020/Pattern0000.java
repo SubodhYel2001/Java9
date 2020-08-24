8c820977a158d6c933b7c6e5166bf90db398edca
SubodhYelgandharwar <yelsubodh2001@gmail.com>
2020-08-24 18:54:19 +0530

    DailyFlash

---
+++ Daily Flash/24-08-2020/Pattern0000.java
@@ -0,0 +1,14 @@
+class Pattern0000 {
+	
+	public static void main(String[] args){
+		
+		for(int i = 1; i <= 4; i++){
+			
+			for(int j = 1; j <= 4; j++){
+				
+				System.out.print("0 ");
+			}
+			System.out.println();
+		}
+	}
+}
